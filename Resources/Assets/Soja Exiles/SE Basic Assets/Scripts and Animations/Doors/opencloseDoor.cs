0eac947726bddbee6205f26be6e1649c0257307c
Cristina <cristina.todor99@gmail.com>
2021-03-08 17:16:57 +0200

    add sprites

---
+++ Resources/Assets/Soja Exiles/SE Basic Assets/Scripts and Animations/Doors/opencloseDoor.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class opencloseDoor: MonoBehaviour {
+
+	public Animator openandclose;
+	public bool open;
+	public Transform Player;
+
+	void Start (){
+		open = false;
+		Player = PlayerManager.instance.player.transform;
+	}
+
+	void OnMouseOver (){
+		{
+			if (Player) {
+				float dist = Vector3.Distance (Player.position, transform.position);
+				if (dist < 15) {
+					if (open == false) {
+						if (Input.GetMouseButtonDown (0)) {
+							StartCoroutine (opening ());
+						}
+					} else {
+						if (open == true) {
+							if (Input.GetMouseButtonDown (0)) {
+								StartCoroutine (closing ());
+							}
+						}
+
+					}
+
+				}
+			}
+
+		}
+
+	}
+
+	IEnumerator opening(){
+		print ("you are opening the door");
+		openandclose.Play ("Opening");
+		FindObjectOfType<AudioManager>().Play("Door2");
+		open = true;
+		yield return new WaitForSeconds (.5f);
+	}
+
+	IEnumerator closing(){
+		print ("you are closing the door");
+		openandclose.Play ("Closing");
+		FindObjectOfType<AudioManager>().Play("Door2");
+		open = false;
+		yield return new WaitForSeconds (.5f);
+	}
+
+
+}
+
---
+++ Resources/Assets/Soja Exiles/SE Basic Assets/Scripts and Animations/Drawer/Z Axis/Drawer_Pull_Z.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawer_Pull_Z: MonoBehaviour {
+
+	public Animator pull;
+	public bool open;
+	public Transform Player;
+
+	void Start (){
+		open = false;
+		Player = PlayerManager.instance.player.transform;
+	}
+
+	void OnMouseOver (){
+		{
+			if (Player) {
+				float dist = Vector3.Distance (Player.position, transform.position);
+				if (dist < 10) {
+                    print("object name");
+					if (open == false) {
+						if (Input.GetMouseButtonDown (0)) {
+							StartCoroutine (opening ());
+						}
+					} else {
+						if (open == true) {
+							if (Input.GetMouseButtonDown (0)) {
+								StartCoroutine (closing ());
+							}
+						}
+
+					}
+
+				}
+			}
+
+		}
+
+	}
+
+	IEnumerator opening(){
+		print ("you are opening the door");
+		pull.Play ("openpull");
+		FindObjectOfType<AudioManager>().Play("Squeak");
+		open = true;
+		yield return new WaitForSeconds (.5f);
+	}
+
+	IEnumerator closing(){
+		print ("you are closing the door");
+		pull.Play ("closepush");
+		FindObjectOfType<AudioManager>().Play("Squeak");
+		open = false;
+		yield return new WaitForSeconds (.5f);
+	}
+
+
+}
+
